2ef174371e69f57abe19411ffaf4f1a4079e3041
kaye.holanda@somosmesha.com <kaye.holanda@somosmesha.com>
2023-01-04 16:16:20 -0300

    Desativa filtro no dicionario

--- components/Dicionario/Dicionario.js
+++ components/Dicionario/Dicionario.js
@@ -1,31 +1,29 @@
 import { FlatList, View, TouchableOpacity, StyleSheet } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
-import { useEffect, useState } from 'react';
+import { useState, useMemo } from 'react';
 
 import Sigla from './Sigla.js';
 import { Filtro } from './Filtro.js';
-import { siglas } from '../../utils/siglas.js';
+import { siglasTags, siglasOrdenadas } from '../../utils/siglas.js';
 
 export default function Dicionario({ navigation }) {
-    const [isFiltroOpen, setFiltroOpen] = useState(false);
-    const [filtro, setFiltro] = useState('');
+    // const [isFiltroOpen, setFiltroOpen] = useState(false);
+    // const [filtro, setFiltro] = useState('');
 
-    const siglasTags = [...new Set(siglas.map(sigla => sigla.tag))];
-    
-    const siglasOrdenadas = siglas.sort((a,b) => a.titulo.localeCompare(b.titulo));
-    const siglasFiltradas = filtro !== '' ? siglasOrdenadas.filter((sigla) => sigla.tag == filtro) : siglasOrdenadas;
+    const memoizedValue = useMemo(() => renderItem, siglasOrdenadas);
 
     const renderItem = ({ item }) => (
         <TouchableOpacity onPress={() => navigation.navigate('SiglaDetalhe', { id: item.id })}>
             <Sigla titulo={item.titulo} />
         </TouchableOpacity>
     );
-
-    const handleChange = (itemValue) => itemValue == 'default' ? setFiltro('') : setFiltro(itemValue);
+    // const handleChange = (itemValue) => itemValue == 'default' ? setFiltro('') : setFiltro(itemValue);
+    
+    // const siglasFiltradas = filtro !== '' ? siglasOrdenadas.filter((sigla) => sigla.tags.join(',') == filtro) : siglasOrdenadas;
 
     return (
         <View style={ styles.container }>
-            <TouchableOpacity onPress={() => setFiltroOpen(!isFiltroOpen)}>
+            {/* <TouchableOpacity onPress={() => setFiltroOpen(!isFiltroOpen)}>
                 <Filtro />
             </TouchableOpacity>
             {isFiltroOpen &&
@@ -38,13 +36,19 @@ export default function Dicionario({ navigation }) {
                         style={ styles.picker }
                     >
                         <Picker.Item label='Nenhum' value='default' />
-                        {siglasTags.map((tag) => <Picker.Item key={tag.indexOf()} label={tag} value={tag} />)}
+                        {siglasTags.map((tags, idx) => <Picker.Item key={idx} label={tags.join(',')} value={tags.join(',')} />)}
                     </Picker>
                 </View> 
-            }
+            } */}
             <FlatList 
-                data={siglasFiltradas}
+                // data={siglasFiltradas}
+                maxToRenderPerBatch={10}
+                updateCellsBatchingPeriod={50}
+                initialNumToRender={10}
+                windowSize={11}
+                data={siglasOrdenadas}
                 renderItem={renderItem}
+                // renderItem={memoizedValue}
                 keyExtractor={item => item.id}
                 style={ styles.lista }
             />
